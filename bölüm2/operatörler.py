5b90fc2e44256d36cfa9b052ff3b9434ce91cc4c
azraatabay <azraatabay21@gmail.com>
2025-09-07 21:29:40 +0300

    python notlarım

---
+++ bölüm2/operatörler.py
@@ -0,0 +1,18 @@
+print(3+4) #toplama
+print(4-1) #çıkarma
+print(4*1) #çarpma
+print(4/2) #ondalıklı bölme
+print(4//3) #tam bölme sadece tam sasyı kısmını alır
+print(4**2) #üs alma
+print(6%4) #mod alır kalanı verir
+
+#birden fazla yapılan işlemlerde normal matematiksel işlem öncelikleri geçerlidir
+#pozitif negatif sayılar için işaretleri konulur -1, 1 gibi
+
+#işlemlerdeki atamalarda sağdan sola atama vardır
+boy=5
+boy=boy+3
+print(boy)
+boy+=3
+print(boy)
+#burada boy değerinin üstüne 3 ekleyerek yeni boy değeri oluşturulur
