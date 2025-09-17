cb45fabccabcc8969386e850bdd2e133eeff7fdc
azraatabay <azraatabay21@gmail.com>
2025-09-17 13:55:49 +0300

    python notlarım

--- bölüm3/atlama_ifadeleri.py
+++ bölüm3/atlama_ifadeleri.py
@@ -7,9 +7,37 @@ while True:
     if sayi<=0:
         break
     toplam+=sayi
+print("toplam:",toplam)
 
-kelime=("bilgisayar")
-for i in range(kelime):
+
+kelime="bilgisayar"
+for i in kelime:
     if i=="s":
         break
     print(i)
+
+#continue
+#döngüyü tamamen sonlandırmaz sadece mevcut adımı sonlandırır
+for i in range(10): #0 ile 9 arasındaki sayıları i değerine atayarak döner
+    if not i%2==0:  #eğer i değerinin iki ile bölümünden kalan 0 değil ise...
+        continue   # o sayıyı ekrana yazdırmaz geç der o adımda döngü yazdırılmadan tekrar başa döner
+    print(i)
+
+cumle="merhaba ben Azra bilgisayar mühendiliği ikinci sınıf öğrencisiyim"
+harf="aeuüoöıi"
+for i in cumle: #i cümle içinde dönüyor
+    if i in harf: #eğer i harf içindekinde bulunuyorsa
+        continue #geç
+    print(i)
+
+#pass
+#hiçbir şey yapmadan devam et anlamına gelir
+rakamlar="2479"
+while True:
+    deneme=input("Bir rakam deneyiniz:")
+    if rakamlar not in deneme:
+        pass
+
+
+
+
